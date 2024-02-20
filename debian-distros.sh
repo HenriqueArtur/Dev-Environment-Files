2a9efe70ab693e88e7453529fe973f032e76c3f1
Henrique Artur <contato@henriqueartur.com>
2024-02-20 20:53:16 -0300

    ✨ install zsh

--- debian-distros.sh
+++ debian-distros.sh
@@ -2,6 +2,9 @@
 
 # SYSTEM
 echo "=> ⏳ Configuring general system behavior"
+echo "--> ⬇ Update System"
+sudo apt update
+sudo apt upgrade -y
 echo "--> 🔄 Change between workspace apps"
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
 echo "> 🔧 Finished configs general systme, behavior"
@@ -14,3 +17,15 @@ echo ""
 echo "=> ⏳ Installing [SPOTIFY (flatpak)]..."
 flatpak install flathub com.spotify.Client
 echo "> 🎵 [SPOTIFY (flatpak)] instaled."
+
+echo ""
+echo "--------------------"
+echo ""
+
+# ZSH
+echo "=> ⏳ Installing [ZSH]..."
+sudo apt install zsh -y
+sudo chsh -s $(which zsh)
+cp .zshrc ~/.zshrc
+echo "> 👾 [ZSH] instaled."
+
