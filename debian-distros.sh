54d8a5d18a2205496fad0096c2658ea1322f8bc4
Henrique Artur <contato@henriqueartur.com>
2024-02-20 20:26:40 -0300

    ✨ Add change between workspaces apps

--- debian-distros.sh
+++ debian-distros.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+# SYSTEM
+echo "=> ⏳ Configuring general system behavior"
+echo "--> 🔄 Change between workspace apps"
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+echo "> 🔧 Finished configs general systme, behavior"
+
+echo ""
+echo "--------------------"
+echo ""
+
 # SPOTIFY
-echo "⏳ Installing [SPOTIFY (flatpak)]..."
+echo "=> ⏳ Installing [SPOTIFY (flatpak)]..."
 flatpak install flathub com.spotify.Client
-echo "🎵 [SPOTIFY (flatpak)] instaled."
+echo "> 🎵 [SPOTIFY (flatpak)] instaled."
