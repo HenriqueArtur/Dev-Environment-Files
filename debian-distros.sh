31802da78e69c94f959c2a9e45efd6862abf0add
Henrique Artur <contato@henriqueartur.com>
2024-02-20 21:37:48 -0300

    ✨ add p10k theme

--- debian-distros.sh
+++ debian-distros.sh
@@ -26,6 +26,28 @@ echo ""
 echo "=> ⏳ Installing [ZSH]..."
 sudo apt install zsh -y
 sudo chsh -s $(which zsh)
-cp .zshrc ~/.zshrc
 echo "> 👾 [ZSH] instaled."
+
+echo ""
+echo "--------------------"
+echo ""
+
+# Powerlevel10k
+echo "=> ⏳ Installing [Powerlevel10k]..."
+echo "--> Installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "--> Installing Zsh plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone https://github.com/marlonrichert/zsh-autocomplete.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+echo "--> Copy Oh My Zsh configs..."
+cp .zshrc ~/.zshrc
+echo "--> Installing p10k theme..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "--> Copy p10k configs for root..."
+cp .p10k.zsh ~/.p10k.zsh
+echo "> 🖌 [Powerlevel10k] instaled."
 
