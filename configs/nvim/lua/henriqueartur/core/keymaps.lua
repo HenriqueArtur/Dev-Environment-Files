c9aed6f6b0ac93818e5c95f9b2339005324e8cdf
Henrique Artur <contato@henriqueartur.com>
2024-03-05 21:12:47 -0300

    🔧 add keymaps to specter and gitmoji

--- configs/nvim/lua/henriqueartur/core/keymaps.lua
+++ configs/nvim/lua/henriqueartur/core/keymaps.lua
@@ -11,3 +11,22 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- nvim-spectre
+vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
+  desc = "Toggle Spectre",
+})
+vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+  desc = "Search current word",
+})
+vim.keymap.set("v", "<leader>sw", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+  desc = "Search current word",
+})
+vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+  desc = "Search on current file",
+})
+
+-- Gitmoji
+vim.keymap.set("n", "<leader>G", ":Telescope gitmoji<CR>", {
+  desc = "Gitmoji",
+})
